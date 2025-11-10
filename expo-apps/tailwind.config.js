599318d471187cfbad59f487474f8099d6a01bbf
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-10 09:56:43 +0000

    feat: enhance tailwind configuration with additional content paths and theme customization

--- expo-apps/tailwind.config.js
+++ expo-apps/tailwind.config.js
@@ -1,10 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FE8C00",
+        white: {
+          DEFAULT: "#FFFFFF",
+          100: "#FAFAFA",
+        },
+        grey: {
+          100: "#878787",
+        },
+        dark: {
+          100: "#181C2E",
+        },
+        error: "#F14141",
+        success: "#2F9865",
+      },
+      fontFamily: {
+        quicksand: ["Quicksand-Regular", "sans-serif"],
+        bold: ["Quicksand-Bold", "sans-serif"],
+        semibold: ["Quicksand-SemiBold", "sans-serif"],
+        light: ["Quicksand-Light", "sans-serif"],
+        medium: ["Quicksand-Medium", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
