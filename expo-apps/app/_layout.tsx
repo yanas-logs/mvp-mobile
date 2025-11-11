feef9827b879b50a6b454f33299f092a9a812c80
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-11 16:18:02 +0000

    feat: add font loading and splash screen handling in RootLayout

--- expo-apps/app/_layout.tsx
+++ expo-apps/app/_layout.tsx
@@ -1,6 +1,18 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+  });
+  useEffect(() => {
+    if (Error) throw Error;
+    if (!fontsLoaded) SplashScreen.hideAsync()
+    }, [fontsLoaded, Error]);
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
