a8dd5ad7ba8c3ac1ef0b9d950df1da6295162e34
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-14 17:36:36 +0000

    feat: enhance offer card layout with conditional styling and icon integration

--- expo-apps/app/index.tsx
+++ expo-apps/app/index.tsx
@@ -2,6 +2,8 @@ import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { offers } from "@/constants";
 import React, { Fragment } from "react";
+import cn from "clsx";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   return (
@@ -9,15 +11,31 @@ export default function Index() {
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
 
           return (
             <View>
-              <Pressable className="offer-card" style={{ backgroundColor: item.color }}>
+              <Pressable
+                className={cn("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+                style={{ backgroundColor: item.color }}
+              >
+
                 {({ pressed }) => (
                   <Fragment>
                     <View className={"h-full w-1/2"}>
                       <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
                     </View>
+
+                    <View className="offer-card__info">
+                      <Text className="h1-bold text-white leading-tight">{item.title}</Text>
+                    </View>
+
+                    <Ionicons
+                      name="arrow-forward"
+                      size={24}
+                      color="white"
+                    />
+
                   </Fragment>
                 )}
               </Pressable>
