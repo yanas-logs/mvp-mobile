62091633bd42331876717ea04602ec0969b52e34
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-15 00:20:47 +0000

    chore: refactor CartButton

--- expo-apps/components/CartButton.tsx
+++ expo-apps/components/CartButton.tsx
@@ -1,12 +1,13 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { images } from '@/constants';
+import { Ionicons } from '@expo/vector-icons';
 
 const CartButton = () => {
     const totalItems = 10;
     return (
         <TouchableOpacity className="cart-btn" onPress={() => { }}>
-            <Image source={images.bag} className="size-5" resizeMode="contain" />
+           <Ionicons name="cart" size={5} color="black" />
             {totalItems > 0 && (
                 <View className="cart-badge">
                     <Text className="small-bold text-white ">{totalItems}</Text>
