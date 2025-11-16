68a2d7b24fd846950174e436e45ea1789bcbe4d9
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2025-11-16 17:33:35 +0000

    chore: add bypass login

--- expo-apps/app/(tabs)/_layout.tsx
+++ expo-apps/app/(tabs)/_layout.tsx
@@ -3,10 +3,11 @@ import { useAuth } from "../../store/auth/AuthContext";
 
 export default function TabsLayout() {
   const { user, loading } = useAuth();
+  const DEV_MODE = true;
 
   if (loading) return null;
 
-  if (!user) return <Redirect href="/login" />;
+if (!user && !DEV_MODE) return <Redirect href="/login" />;
 
   return (
     <Tabs screenOptions={{ headerShown: false }}>
